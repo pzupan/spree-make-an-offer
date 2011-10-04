50fdbc3b9f8bc534d8ffe4eb1da370ee94c1c77a
Paul Zupan <pzupan@gmail.com>
2011-10-03 17:09:04 -0700

    fix bug in admin menu

--- app/controllers/admin/configurations_controller_decorator.rb
+++ app/controllers/admin/configurations_controller_decorator.rb
@@ -3,6 +3,6 @@ Admin::ConfigurationsController.class_eval do
   before_filter :add_offers_link, :only => :index
  
   def add_offers_link
-    @extension_links << {:link => admin_offers_url, :link_text => t('offers'), :description => t('offers_description')}
+#    @extension_links << {:link => admin_offers_url, :link_text => t('offers'), :description => t('offers_description')}
   end
 end
