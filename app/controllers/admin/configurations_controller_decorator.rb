f395edb79fd237b44e6b203f9c6ce2313217a2bc
Paul Zupan <pzupan@gmail.com>
2011-10-06 10:06:32 -0700

    clean up admin

--- app/controllers/admin/configurations_controller_decorator.rb
+++
@@ -1,8 +0,0 @@
-Admin::ConfigurationsController.class_eval do
-
-  before_filter :add_offers_link, :only => :index
- 
-  def add_offers_link
-#    @extension_links << {:link => admin_offers_url, :link_text => t('offers'), :description => t('offers_description')}
-  end
-end
